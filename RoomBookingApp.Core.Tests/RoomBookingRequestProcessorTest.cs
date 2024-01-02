9987c9b498da50ea89fe38f9ed392a1d5423682e
Arturs Krusts <arturs@atmosclear.com>
2024-01-02 13:50:20 +0200

    Add project files.

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,31 @@
+﻿using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+using Xunit;
+
+namespace RoomBookingApp.Core.Tests
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void ShouldReturnRoomBookingResponseWithRequestValues()
+        {
+            //Arrange
+            var bookingRequest = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@test.com",
+                Date = new DateTime(2022, 12, 12)
+            };
+            var processor = new RoomBookingRequestProcessor();
+
+            //Act
+            RoomBookingResult result = processor.BookRoom(bookingRequest);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(bookingRequest.FullName, result.FullName);
+            Assert.Equal(bookingRequest.Email, result.Email);
+            Assert.Equal(bookingRequest.Date, result.Date);
+        }
+    }
+}
